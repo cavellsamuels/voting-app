6c214590909a5dd2feb7d3acf7bacbbe2cf3efde
Cavell Singleton <cavellsingleton@IT-LAP-006.ManningStainton.local>
2022-09-27 12:22:34 +0100

    Initial Design Setup

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,33 +1,85 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+    <title> Voting </title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Open+Sans:wght@400;600;700&display=swap">
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans text-gray-900 text-sm">
+    <header class="flex items-center justify-between px-8 py-4">
+        <a href="#"><img src="{{ asset('img/logo.svg') }}" alt=""></a>
+        <div class="flex items-center">
+            @if (Route::has('login'))
+                <div class="px-6 py-4">
+                    @auth
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <a href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                {{ __('Log out') }}
+                            </a>
+                        </form>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endauth
                 </div>
-            </header>
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            @endif
+            <a href="#">
+                <img src="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp" alt="avatar"
+                    class="w-10 h-10 rounded-full">
+            </a>
         </div>
-    </body>
+    </header>
+
+    <main class="container mx-auto max-w-custom flex" style="max-width: 1000px;">
+        <div class="w-70 mr-5">
+            Add Idea Form Here. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat dolorum eaque
+            pariatur laudantium quod natus hic cupiditate quisquam unde non tenetur deserunt modi, doloremque, sapiente
+            in ad omnis quidem consequuntur tempore illo nesciunt? Sapiente consequuntur rerum fugiat id necessitatibus
+            officiis.
+        </div>
+        <div class="w-175">
+            <nav class="flex items-center justify-between text-xs">
+                <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                    <li><a href="#" class="border-b-4 pb-3 border-blue"> All Ideas (87) </a></li>
+                    <li><a href="#"
+                            class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">
+                            Considering (6) </a></li>
+                    <li><a href="#"
+                            class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue"> In
+                            Progress (1) </a></li>
+                </ul>
+                <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                    <li><a href="#"
+                            class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">
+                            Implemented (10) </a></li>
+                    <li><a href="#"
+                            class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">
+                            Closed (55) </a></li>
+                </ul>
+            </nav>
+
+            <div class="mt-8">
+                {{ $slot }}
+            </div>
+        </div>
+    </main>
+</body>
+
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -10,8 +11,33 @@ module.exports = {
 
     theme: {
         extend: {
+            colors: {
+                transparent: colors.transparent,
+                current: colors.current,
+
+                black: colors.black,
+                white: colors.white,
+                gray: colors.trueGray,
+                'gray-background': '#f7f8fc',
+                'blue': '#328af1',
+                'blue-hover': '#2879bd',
+                'yellow': '#ffc73c',
+                'red': '#ec454f',
+                'green': '#1aab8b',
+                'purple': '#8b60ed',
+            },
+
+            spacing: {
+                70: '17.5rem',
+                175: '43.75rem'
+            },
+
+            maxWidth: {
+                custom: '62.5rem'
+            },
+
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
             },
         },
     },
