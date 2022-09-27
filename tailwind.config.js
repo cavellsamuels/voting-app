878ccef2d7338ba39f367fbd7374f046dc486816
Cavell Singleton <cavellsingleton@IT-LAP-006.ManningStainton.local>
2022-09-27 14:50:21 +0100

    Add Idea Section and Idea Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,13 +28,14 @@ module.exports = {
             },
 
             spacing: {
+                22: '5.5rem',
                 44: '11rem',
                 70: '17.5rem',
                 175: '43.75rem'
             },
 
             maxWidth: {
-                custom: '62.5rem'
+                custom: '68.5rem',
             },
 
             boxShadow: {
